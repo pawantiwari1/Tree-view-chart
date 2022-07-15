29b6bc9d82a15a1f687cc720637ceb6d4b2a0ddd
pawantiwari1 <tiwaripawan388@gmail.com>
2022-07-15 16:23:41 +0530

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { NgxOrgChartModule } from 'ngx-org-chart';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { D3OrgChartComponent } from './d3-org-chart/d3-org-chart.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, D3OrgChartComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, NgxOrgChartModule],
+  declarations: [AppComponent, HelloComponent, D3OrgChartComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/d3-org-chart/d3-org-chart.component.ts
+++ src/app/d3-org-chart/d3-org-chart.component.ts
@@ -4,23 +4,25 @@ import {
   OnInit,
   Input,
   ViewChild,
-  ElementRef
-} from "@angular/core";
-import  TreeChart from "d3-org-chart";
+  ElementRef,
+} from '@angular/core';
+import TreeChart from 'd3-org-chart';
 
 @Component({
-  selector: "app-d3-org-chart",
-  templateUrl: "./d3-org-chart.component.html",
-  styleUrls: ["./d3-org-chart.component.css"]
+  selector: 'app-d3-org-chart',
+  templateUrl: './d3-org-chart.component.html',
+  styleUrls: ['./d3-org-chart.component.css'],
 })
 export class D3OrgChartComponent implements OnInit, OnChanges {
-  @ViewChild("chartContainer") chartContainer: ElementRef;
+  @ViewChild('chartContainer') chartContainer: ElementRef;
   @Input() data: any[];
   chart;
-
+  nodes: any;
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.createngxorgChart();
+  }
 
   ngAfterViewInit() {
     if (!this.chart) {
@@ -34,17 +36,195 @@ export class D3OrgChartComponent implements OnInit, OnChanges {
   }
   updateChart() {
     if (!this.data) {
-      return; 
+      return;
     }
     if (!this.chart) {
-      return; 
+      return;
     }
     this.chart
       .container(this.chartContainer.nativeElement)
       .data(this.data)
       .svgWidth(500)
       .initialZoom(0.4)
-      .onNodeClick(d => console.log(d + " node clicked"))
+      .onNodeClick((d) => console.log(d + ' node clicked'))
       .render();
+  }
+  createngxorgChart() {
+    this.nodes = [
+      {
+        name: 'Sundar Pichai',
+        cssClass: 'ngx-org-ceo',
+        image: '',
+        title: 'Chief Executive Officer',
+        childs: [
+          {
+            name: 'Thomas Kurian',
+            cssClass: 'ngx-org-ceo',
+            image: 'assets/node.svg',
+            title: 'CEO, Google Cloud',
+          },
+          {
+            name: 'Susan Wojcicki',
+            cssClass: 'ngx-org-ceo',
+            image: 'assets/node.svg',
+            title: 'CEO, YouTube',
+            childs: [
+              {
+                name: 'Beau Avril',
+                cssClass: 'ngx-org-head',
+                image: 'assets/node.svg',
+                title: 'Global Head of Business Operations',
+                childs: [],
+              },
+              {
+                name: 'Tara Walpert Levy',
+                cssClass: 'ngx-org-vp',
+                image: 'assets/node.svg',
+                title: 'VP, Agency and Brand Solutions',
+                childs: [],
+              },
+              {
+                name: 'Ariel Bardin',
+                cssClass: 'ngx-org-vp',
+                image: 'assets/node.svg',
+                title: 'VP, Product Management',
+                childs: [],
+              },
+            ],
+          },
+          {
+            name: 'Jeff Dean',
+            cssClass: 'ngx-org-head',
+            image: 'assets/node.svg',
+            title: 'Head of Artificial Intelligence',
+            childs: [
+              {
+                name: 'David Feinberg',
+                cssClass: 'ngx-org-ceo',
+                image: 'assets/node.svg',
+                title: 'CEO, Google Health',
+                childs: [],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: 'Sundar Pichai',
+        cssClass: 'ngx-org-ceo',
+        image: 'assets/node.svg',
+        title: 'Chief Executive Officer',
+        childs: [
+          {
+            name: 'Thomas Kurian',
+            cssClass: 'ngx-org-ceo',
+            image: 'assets/node.svg',
+            title: 'CEO, Google Cloud',
+          },
+          {
+            name: 'Susan Wojcicki',
+            cssClass: 'ngx-org-ceo',
+            image: 'assets/node.svg',
+            title: 'CEO, YouTube',
+            childs: [
+              {
+                name: 'Beau Avril',
+                cssClass: 'ngx-org-head',
+                image: 'assets/node.svg',
+                title: 'Global Head of Business Operations',
+                childs: [],
+              },
+              {
+                name: 'Tara Walpert Levy',
+                cssClass: 'ngx-org-vp',
+                image: 'assets/node.svg',
+                title: 'VP, Agency and Brand Solutions',
+                childs: [],
+              },
+              {
+                name: 'Ariel Bardin',
+                cssClass: 'ngx-org-vp',
+                image: 'assets/node.svg',
+                title: 'VP, Product Management',
+                childs: [],
+              },
+            ],
+          },
+          {
+            name: 'Jeff Dean',
+            cssClass: 'ngx-org-head',
+            image: 'assets/node.svg',
+            title: 'Head of Artificial Intelligence',
+            childs: [
+              {
+                name: 'David Feinberg',
+                cssClass: 'ngx-org-ceo',
+                image: 'assets/node.svg',
+                title: 'CEO, Google Health',
+                childs: [],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: 'Sundar Pichai',
+        cssClass: 'ngx-org-ceo',
+        image: 'assets/node.svg',
+        title: 'Chief Executive Officer',
+        childs: [
+          {
+            name: 'Thomas Kurian',
+            cssClass: 'ngx-org-ceo',
+            image: 'assets/node.svg',
+            title: 'CEO, Google Cloud',
+          },
+          {
+            name: 'Susan Wojcicki',
+            cssClass: 'ngx-org-ceo',
+            image: 'assets/node.svg',
+            title: 'CEO, YouTube',
+            childs: [
+              {
+                name: 'Beau Avril',
+                cssClass: 'ngx-org-head',
+                image: 'assets/node.svg',
+                title: 'Global Head of Business Operations',
+                childs: [],
+              },
+              {
+                name: 'Tara Walpert Levy',
+                cssClass: 'ngx-org-vp',
+                image: 'assets/node.svg',
+                title: 'VP, Agency and Brand Solutions',
+                childs: [],
+              },
+              {
+                name: 'Ariel Bardin',
+                cssClass: 'ngx-org-vp',
+                image: 'assets/node.svg',
+                title: 'VP, Product Management',
+                childs: [],
+              },
+            ],
+          },
+          {
+            name: 'Jeff Dean',
+            cssClass: 'ngx-org-head',
+            image: 'assets/node.svg',
+            title: 'Head of Artificial Intelligence',
+            childs: [
+              {
+                name: 'David Feinberg',
+                cssClass: 'ngx-org-ceo',
+                image: 'assets/node.svg',
+                title: 'CEO, Google Health',
+                childs: [],
+              },
+            ],
+          },
+        ],
+      },
+    ];
   }
 }
